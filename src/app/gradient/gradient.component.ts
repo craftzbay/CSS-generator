55d8c7add9d3fce8a85ed56fc7b587d97dcf27db
Bayraa <bayraa.3033@gmail.com>
2019-07-21 21:17:40 +0800

    gradient generator huudas shiljihed ygad prismiin css ajillahgui bn we

--- src/app/gradient/gradient.component.ts
+++ src/app/gradient/gradient.component.ts
@@ -9,6 +9,10 @@ export class GradientComponent implements OnInit {
   color1 = '#ffffff3a';
   color2 = '#1d2749ff';
   direction = 'to left';
+  arrow = 1;
+  selectArrow(id){
+    this.arrow = id
+  }
   constructor() { }
 
   ngOnInit() {
