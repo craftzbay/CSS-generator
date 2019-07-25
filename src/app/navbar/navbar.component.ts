1bbea318e568dda23fc19196b9cc311f986528fd
Bayarsaikhan <bayraa.3033@gmail.com>
2019-07-25 11:34:48 +0800

    style

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  title: string = 'CSS3'
-  constructor() { }
+  title: string = 'HOME';
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
