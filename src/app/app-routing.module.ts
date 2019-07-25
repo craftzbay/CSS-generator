8e30fdcb3d6ff4f02e2703d7b06d09c3c92ff09e
Bayarsaikhan <bayraa.3033@gmail.com>
2019-07-25 11:51:50 +0800

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GradientComponent } from './gradient/gradient.component';
 import { BoxshadowComponent } from './boxshadow/boxshadow.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: GradientComponent },
+  { path: '', component: HomeComponent },
   { path: 'gradient', component: GradientComponent },
   { path: 'box-shadow', component: BoxshadowComponent }
 ];
