1837d58e54d7cad60fb0ebf84944baf4bc65ad42
Bayarsaikhan <bayraa.3033@gmail.com>
2019-07-25 11:52:05 +0800

    navbar contained

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  title: string = 'HOME';
+  title: string = 'Home';
   constructor() {}
 
   ngOnInit() {}
